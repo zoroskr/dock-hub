d3692d8c79d6bf66fb1412eb60aa7e5d41fc8b8d
Felipe Fidalgo <elmundosegunfelicapo@hotmail.com>
2024-05-01 18:33:47 -0300

    prueb1

--- src/app/layout.js
+++ src/app/layout.js
@@ -11,7 +11,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>{children}
+      <script src="../path/to/flowbite/dist/flowbite.min.js"></script>
+      </body>
     </html>
   );
 }
