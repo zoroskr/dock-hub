7a182abeff8f705614911aa24e69085bf2a42132
Felipe Fidalgo <elmundosegunfelicapo@hotmail.com>
2024-05-05 17:16:54 -0300

    agregue iniciar sesion, registrarse y el schema de mongoose

--- src/app/page.js
+++ src/app/page.js
@@ -4,10 +4,14 @@ import Publicacion from "@/components/ui/Publicacion";
 export default function Home() {
   const titulos = ["Título 1", "Título 2", "Título 3", "Título 4", "Título 5"];
   return (
-    <div> 
-      <Navbar></Navbar>
-      
-      {titulos.map((titulo) => (<Publicacion title={titulo}></Publicacion>))}
-    </div>
+      <div className="flex flex-col justify-center items-center">
+        <div className="flex flex-col justify-center items-center max-w-7xl">
+          <div className="flex flex-wrap">
+            {titulos.map((titulo, index) => (
+              <Publicacion title={titulo} key={index}></Publicacion>
+            ))}
+          </div>
+        </div>
+      </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,7 @@ module.exports = {
     './app/**/*.{js,jsx}',
     './src/**/*.{js,jsx}',
     "./src/**/*.{html,js}",
+    "./node_modules/flowbite/**/*.js"
   ],
   prefix: "",
   theme: {
