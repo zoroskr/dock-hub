424b474b78b0a9b404cf950377853b9d5b883280
Ivan Skrt <ivanskrt1@gmail.com>
2024-05-12 17:50:19 -0300

    cambios ivan 12/05

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -38,18 +38,18 @@ export default function Home() {
   };
 
   return (
-    <section class="bg-gray-50 dark:bg-gray-900">
+    <section class="bg-rgb-190-175-85  dark:bg-gray-900">
       <div class="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
-        <div class="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+         <div className="w-full md:max-w-md lg:max-w-md xl:max-w-lg bg-gray-900 rounded-xl shadow dark:border dark:bg-gray-800 dark:border-gray-700">
           <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
-            <h1 class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
+            <h1 class="text-xl font-bold leading-tight tracking-tight text-white md:text-2xl dark:text-white">
               Iniciar sesión
             </h1>
             <form class="space-y-4 md:space-y-6" onSubmit={handleLogin}>
               <div>
                 <label
                   for="email"
-                  class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                  class="block mb-2 text-sm font-medium text-white dark:text-white"
                 >
                   Email
                 </label>
@@ -58,7 +58,7 @@ export default function Home() {
                   type="email"
                   name="email"
                   id="email"
-                  class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  class="bg-gray-50 border border-gray-300 text-black sm:text-sm rounded-xl focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                   placeholder="pancracio@example.com"
                   required=""
                 />
@@ -66,7 +66,7 @@ export default function Home() {
               <div>
                 <label
                   for="password"
-                  class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                  class="block mb-2 text-sm font-medium text-white dark:text-white"
                 >
                   Contraseña
                 </label>
@@ -76,7 +76,7 @@ export default function Home() {
                   name="password"
                   id="password"
                   placeholder="••••••••"
-                  class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  class="bg-gray-50 border border-gray-300 text-black sm:text-sm rounded-xl focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                   required=""
                 />
               </div>
@@ -90,15 +90,15 @@ export default function Home() {
               </div> */}
               <button
                 type="submit"
-                class="w-full text-black bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
+                class="w-full text-black bg-custom-yellow hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-xl text-sm px-5 py-2.5 text-center"
               >
                 Iniciar sesión
               </button>
-              <p class="text-sm font-light text-gray-500 dark:text-gray-400">
+              <p class="text-sm font-light text-white dark:text-gray-400">
                 No tienes una cuenta?{" "}
                 <Link
                   href="/register"
-                  class="font-medium text-primary-600 hover:underline dark:text-primary-500"
+                  class="font-medium text-primary-600 hover:underline dark:text-primary-500 "
                 >
                   Registrarse
                 </Link>
--- src/components/ui/UserForm.jsx
+++ src/components/ui/UserForm.jsx
@@ -81,99 +81,103 @@ const UserForm = ({ user, title }) => {
   };
 
   return (
-    <form ref={form} class="max-w-sm mx-auto" onSubmit={handleSubmit}>
-      <h1 class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
-        {title}
-      </h1>
-      <div class="mb-5">
-        <label
-          for="fullname"
-          class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-        >
-          Nombre
-        </label>
-        <input
-          type="text"
-          id="fullname"
-          name="fullName" // Add this line
-          class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light"
-          placeholder="John Doe"
-          required
-        />
-      </div>
+    <div className="flex justify-center items-center h-screen">
+      <div className="max-w-lg w-full bg-gray-900 rounded-xl p-6">
+        <form ref={form} class="max-w-sm mx-auto" onSubmit={handleSubmit}>
+          <h1 class="text-xl font-bold leading-tight tracking-tight text-white md:text-2xl dark:text-white">
+            {title}
+          </h1>
+          <div class="mb-5">
+            <label
+              for="fullname"
+              class="block mb-2 text-sm font-medium text-white dark:text-white"
+            >
+              Nombre
+            </label>
+            <input
+              type="text"
+              id="fullname"
+              name="fullName" // Add this line
+              class="shadow-sm bg-gray-50 border border-gray-300 text-black text-sm rounded-xl focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light"
+              placeholder="John Doe"
+              required
+            />
+          </div>
 
-      <div class="mb-5">
-        <label
-          for="address"
-          class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-        >
-          Dirección
-        </label>
-        <input
-          type="text"
-          id="address"
-          name="address"
-          class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light"
-          placeholder="123 Main St, New York, NY 10030"
-          required
-        />
-      </div>
+          <div class="mb-5">
+            <label
+              for="address"
+              class="block mb-2 text-sm font-medium text-white dark:text-white"
+            >
+              Dirección
+            </label>
+            <input
+              type="text"
+              id="address"
+              name="address"
+              class="shadow-sm bg-gray-50 border border-gray-300 text-white text-sm rounded-xl focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light"
+              placeholder="123 Main St, New York, NY 10030"
+              required
+            />
+          </div>
 
-      <div class="mb-5">
-        <label
-          for="email"
-          class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-        >
-          Email
-        </label>
-        <input
-          type="email"
-          id="email"
-          name="email"
-          class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light"
-          placeholder="tuemail@example.com"
-          required
-        />
-      </div>
+          <div class="mb-5">
+            <label
+              for="email"
+              class="block mb-2 text-sm font-medium text-white dark:text-white"
+            >
+              Email
+            </label>
+            <input
+              type="email"
+              id="email"
+              name="email"
+              class="shadow-sm bg-gray-50 border border-gray-300 text-black text-sm rounded-xl focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light"
+              placeholder="tuemail@example.com"
+              required
+            />
+          </div>
 
-      <div class="mb-5">
-        <label
-          for="password"
-          class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-        >
-          Contraseña
-        </label>
-        <input
-          type="password"
-          id="password"
-          name="password"
-          class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light"
-          required
-        />
-      </div>
-      <div class="mb-5">
-        <label
-          for="repeat-password"
-          class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-        >
-          Repetir contraseña
-        </label>
-        <input
-          type="password"
-          id="repeat-password"
-          name="repeat-password"
-          class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light"
-          required
-        />
-      </div>
+          <div class="mb-5">
+            <label
+              for="password"
+              class="block mb-2 text-sm font-medium text-white dark:text-white"
+            >
+              Contraseña
+            </label>
+            <input
+              type="password"
+              id="password"
+              name="password"
+              class="shadow-sm bg-gray-50 border border-gray-300 text-black text-sm rounded-xl focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light"
+              required
+            />
+          </div>
+          <div class="mb-5">
+            <label
+              for="repeat-password"
+              class="block mb-2 text-sm font-medium text-white dark:text-white"
+            >
+              Repetir contraseña
+            </label>
+            <input
+              type="password"
+              id="repeat-password"
+              name="repeat-password"
+              class="shadow-sm bg-gray-50 border border-gray-300 text-black text-sm rounded-xl focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light"
+              required
+            />
+          </div>
 
-      <button
-        type="submit"
-        class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-      >
-        {title}
-      </button>
-    </form>
+          <button
+            type="submit"
+            class="text-black  bg-custom-yellow duration-300 hover:scale-110 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-xl text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+          >
+            {title}
+          </button>
+        </form>
+      </div>
+    </div>
   );
 }
 
