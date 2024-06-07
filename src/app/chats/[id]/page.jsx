b8a0a2b9b533d620f1bf3f9d8a409a1c9b9ac829
Ivan Skrt <ivanskrt1@gmail.com>
2024-06-07 13:50:14 -0300

    estilaje de chat p1

--- src/app/chats/[id]/page.jsx
+++ src/app/chats/[id]/page.jsx
@@ -1,18 +1,15 @@
 "use client";
 import React, { useState, useEffect } from "react";
-import { useRouter } from "next/navigation";
 import { useParams } from "next/navigation";
 import { getUser } from "../../services/users.api";
 
 const ChatInterface = () => {
   const [currentMessage, setCurrentMessage] = useState("");
   const [messages, setMessages] = useState([]);
-  const [chatId, setChatId] = useState();
+  const [chat, setChat] = useState(null);
   const [user, setUser] = useState({});
   const [chats, setChats] = useState([]);
   const params = useParams();
-
-  // const chatId = params.id; // Asume que la ruta es algo como /chat/[id]
 
   const sendMessage = async (e) => {
     e.preventDefault();
@@ -35,73 +32,74 @@ const ChatInterface = () => {
   };
 
   useEffect(() => {
+    const loadChatAndMessages = async () => {
+      try {
+        const usuario = await getUser(localStorage.getItem("id"));
+        setUser(usuario);
 
-    const loadMessages = async () => {
-      const usuario = await getUser(localStorage.getItem("id"));
-      setUser(usuario);
-      console.log("🚀 ~ loadMessages ~ setUser:", user);
+        const chatResponse = await fetch(`http://localhost:3000/api/chats/${params.id}`);
+        if (chatResponse.ok) {
+          const chatData = await chatResponse.json();
+          setChat(chatData);
 
-      const response = await fetch(`http://localhost:3000/api/chats/${params.id}`);
-      if (response.ok) {
-        const chat = await response.json();
-        console.log(chat.messages);
-
-        const responseAllMessages = await fetch(`http://localhost:3000/api/messages`);
-        if (responseAllMessages.ok) {
-          const allMessages = await responseAllMessages.json();
-          const filteredMessages = allMessages.filter((message) => chat.messages.includes(message._id));
-          setMessages(filteredMessages);
+          const messagesResponse = await fetch(`http://localhost:3000/api/messages`);
+          if (messagesResponse.ok) {
+            const allMessages = await messagesResponse.json();
+            const filteredMessages = allMessages.filter((message) => chatData.messages.includes(message._id));
+            setMessages(filteredMessages);
+          }
         }
+      } catch (error) {
+        console.error("Error loading chat and messages:", error);
       }
-      setChatId(params.id);
     };
 
-    // Cargar los mensajes inmediatamente después de que el componente se monte
-    loadMessages();
+    loadChatAndMessages();
 
-    // Configurar un intervalo para cargar nuevos mensajes cada 5 segundos
-    const intervalId = setInterval(loadMessages, 2000);
-
-    // Limpiar el intervalo cuando el componente se desmonte
+    const intervalId = setInterval(loadChatAndMessages, 2000);
     return () => clearInterval(intervalId);
-  }, []);
+  }, [params.id]);
+
+  if (!chat) return <div className="font-bold">Cargando...</div>;
+
+  const otherUser = chat.users.find((chatUser) => chatUser._id !== user._id);
 
   return (
-    <div className="flex flex-row bg-red-500 ">
-      <ul className="flex justify-center bg-green-500 p-10">
-        {chats.map((chat) => (
-          <li key={chat._id} className="m-4 cursor-pointer">
-            {chat._id}
-          </li>
-        ))}
-      </ul>
-      <div className="w-1/2">
-        <div className="overflow-auto p-4 bg-gray-800 ">
-          {messages.map((message, index) =>
-            message.sender == user.fullName ? (
-              <div key={index} className="flex justify-end">
-                <div className="flex bg-red-600 p-5 m-4 rounded-xl">{message.content}</div>
-              </div>
-            ) : (
-              <div>
-                <div key={index} className="rounded-xl inline-block bg-blue-900 p-5 m-4">
-                  {message.content}
+    <div className="flex justify-end w-full items-center min-h-screen">
+      <div className="flex flex-col w-full max-w-4xl p-10">
+        <ul className="flex justify-center p-3">
+          {chats.map((chat) => (
+            <li key={chat._id} className="m-4 cursor-pointer">
+              {chat._id}
+            </li>
+          ))}
+        </ul>
+        <div className="w-full max-w-lg rounded-xl bg-gray-400">
+          <div className="text-center text-2xl bg-gray-800 text-white font-semibold rounded-t-xl p-3">
+            {otherUser ? `${otherUser.fullName}` : 'Cargando usuario...'}
+          </div>
+          <div className="overflow-auto p-4 bg-gray-800">
+            {messages.map((message, index) => (
+              <div key={index} className={`flex ${message.sender === user.fullName ? 'justify-end' : 'justify-start'}`}>
+                <div className={`bg-custom-yellow text-black p-3 m-4 rounded-xl max-w-xs`}>
+                  <div className="text-sm mb-1 text-black font-bold justify-end">{message.sender}</div>
+                  <div>{message.content}</div>
                 </div>
               </div>
-            ),
-          )}
+            ))}
+          </div>
+          <form onSubmit={sendMessage} className="flex p-4 bg-white border-t border-gray-200 rounded-b-xl">
+            <input
+              className="flex-grow px-4 py-2 mr-4 rounded-xl border border-gray-300"
+              placeholder="Escribe un mensaje..."
+              value={currentMessage}
+              onChange={(e) => setCurrentMessage(e.target.value)}
+            />
+            <button type="submit" className="px-4 py-2 rounded-xl bg-gray-800 duration-300   hover:bg-gray-500 text-white">
+              Enviar
+            </button>
+          </form>
         </div>
-        <form onSubmit={sendMessage} className="flex p-4 bg-white border-t border-gray-200">
-          <input
-            className="flex-grow px-4 py-2 mr-4 rounded-lg border border-gray-300"
-            placeholder="Escribe un mensaje..."
-            value={currentMessage}
-            onChange={(e) => setCurrentMessage(e.target.value)}
-          />
-          <button type="submit" className="px-4 py-2 bg-blue-500 text-white rounded-lg">
-            Enviar
-          </button>
-        </form>
       </div>
     </div>
   );
