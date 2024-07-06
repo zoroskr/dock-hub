16d90a776e24524622c2c16a030946f1c200eb8b
Alexis Rodriguez <alexisrodriguezarteaga@gmail.com>
2024-07-05 23:07:15 -0300

    Fix localStorage

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -36,18 +36,18 @@ export default function Home() {
   };
 
   return (
-    <section class="bg-rgb-190-175-85  dark:bg-gray-900">
-      <div class="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+    <section className="bg-rgb-190-175-85  dark:bg-gray-900">
+      <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
         <div className="w-full md:max-w-md lg:max-w-md xl:max-w-lg bg-gray-900 rounded-xl shadow dark:border dark:bg-gray-800 dark:border-gray-700">
-          <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
-            <h1 class="text-xl font-bold leading-tight tracking-tight text-white md:text-2xl dark:text-white">
+          <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
+            <h1 className="text-xl font-bold leading-tight tracking-tight text-white md:text-2xl dark:text-white">
               Iniciar sesión
             </h1>
-            <form class="space-y-4 md:space-y-6" onSubmit={handleLogin}>
+            <form className="space-y-4 md:space-y-6" onSubmit={handleLogin}>
               <div>
                 <label
-                  for="email"
-                  class="block mb-2 text-sm font-medium text-white dark:text-white"
+                  htmlFor="email"
+                  className="block mb-2 text-sm font-medium text-white dark:text-white"
                 >
                   Email
                 </label>
@@ -56,15 +56,15 @@ export default function Home() {
                   type="email"
                   name="email"
                   id="email"
-                  class="bg-gray-50 border border-gray-300 text-black sm:text-sm rounded-xl focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  className="bg-gray-50 border border-gray-300 text-black sm:text-sm rounded-xl focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                   placeholder="pancracio@example.com"
                   required=""
                 />
               </div>
               <div>
                 <label
-                  for="password"
-                  class="block mb-2 text-sm font-medium text-white dark:text-white"
+                  htmlFor="password"
+                  className="block mb-2 text-sm font-medium text-white dark:text-white"
                 >
                   Contraseña
                 </label>
@@ -74,21 +74,21 @@ export default function Home() {
                   name="password"
                   id="password"
                   placeholder="••••••••"
-                  class="bg-gray-50 border border-gray-300 text-black sm:text-sm rounded-xl focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  className="bg-gray-50 border border-gray-300 text-black sm:text-sm rounded-xl focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                   required=""
                 />
               </div>
               <button
                 type="submit"
-                class="w-full text-black bg-custom-yellow hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-xl text-sm px-5 py-2.5 text-center"
+                className="w-full text-black bg-custom-yellow hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-xl text-sm px-5 py-2.5 text-center"
               >
                 Iniciar sesión
               </button>
-              <p class="text-sm font-light text-white dark:text-gray-400">
+              <p className="text-sm font-light text-white dark:text-gray-400">
                 No tienes una cuenta?{" "}
                 <Link
                   href="/register"
-                  class="font-medium text-primary-600 hover:underline dark:text-primary-500"
+                  className="font-medium text-primary-600 hover:underline dark:text-primary-500"
                 >
                   Regístrate!
                 </Link>
--- src/app/page.js
+++ src/app/page.js
@@ -1,14 +1,15 @@
 "use client";
 
-import React, { useEffect, useRef, useState } from 'react';
-import Post from '@/components/ui/Post';
-import { getPosts } from './services/posts.api';
-import { getUser } from './services/users.api';
+import React, { useEffect, useRef, useState } from "react";
+import Post from "@/components/ui/Post";
+import { getPosts } from "./services/posts.api";
+import { getUser } from "./services/users.api";
+import Loading from "./loading";
 
 export default function Home() {
   const [posts, setPosts] = useState([]);
   const [loading, setLoading] = useState(true);
-  const [message, setMessage] = useState('No hay publicaciones disponibles');
+  const [message, setMessage] = useState("No hay publicaciones disponibles");
   const [favoritesPosts, setFavoritesPosts] = useState([]);
   const [selectedOptions, setSelectedOptions] = useState({
     Embarcaciones: false,
@@ -25,22 +26,25 @@ export default function Home() {
       try {
         let fetchedPosts = await getPosts();
         //filtrar por los que no corresponden al usuario logueado
-        fetchedPosts = fetchedPosts.filter(p => p.owner !== localStorage.getItem('id'));
+        fetchedPosts = fetchedPosts.filter((p) => p.owner !== localStorage.getItem("id"));
         //Ver si se muestran posts de todos los tipos o solo de embarcaciones
-        if (!localStorage.getItem('id')  || localStorage.getItem('verified') == 'false' && localStorage.getItem('type') != 'Admin') {
-          fetchedPosts = fetchedPosts.filter(p => p.type == 'Embarcaciones');
+        if (
+          !localStorage.getItem("id") ||
+          (localStorage.getItem("verified") == "false" && localStorage.getItem("type") != "Admin")
+        ) {
+          fetchedPosts = fetchedPosts.filter((p) => p.type == "Embarcaciones");
         }
         //Mostrar solo posts en estado Activo
-        fetchedPosts = fetchedPosts.filter(p => p.state == 'Activo');
+        fetchedPosts = fetchedPosts.filter((p) => p.state == "Activo");
 
-        if (localStorage.getItem('id')) {
-          user = await getUser(localStorage.getItem('id'));
+        if (localStorage.getItem("id")) {
+          user = await getUser(localStorage.getItem("id"));
           setFavoritesPosts(user.favorites);
         }
         setPosts(fetchedPosts);
         setLoading(false);
       } catch (error) {
-        setMessage('Error al obtener publicaciones');
+        setMessage("Error al obtener publicaciones");
         setLoading(false);
       }
     };
@@ -51,10 +55,10 @@ export default function Home() {
     e.preventDefault();
     const posts = await getPosts(); // Traigo todos los posts de nuevo para buscar desde ahi
 
-    let defaultSearch = '';
+    let defaultSearch = "";
 
     if (form.current) {
-      defaultSearch = form.current.elements['default-search'].value;
+      defaultSearch = form.current.elements["default-search"].value;
     }
 
     const lowerCaseSearch = defaultSearch.toLowerCase();
@@ -63,62 +67,66 @@ export default function Home() {
     if (!defaultSearch) {
       try {
         setPosts(posts);
-        setMessage(posts.length === 0 ? 'No hay publicaciones disponibles' : '');
+        setMessage(posts.length === 0 ? "No hay publicaciones disponibles" : "");
       } catch (error) {
-        console.error('Error fetching posts:', error);
-        setMessage('Error al obtener publicaciones');
+        console.error("Error fetching posts:", error);
+        setMessage("Error al obtener publicaciones");
       }
       return;
     }
 
     // Filtrar los posts existentes
     if (posts.length > 0) {
-      const postsFiltered = posts.filter(p => 
-        (p.name.toLowerCase().includes(lowerCaseSearch) || p.description.toLowerCase().includes(lowerCaseSearch)) && p.owner !== localStorage.getItem('id')
+      const postsFiltered = posts.filter(
+        (p) =>
+          (p.name.toLowerCase().includes(lowerCaseSearch) || p.description.toLowerCase().includes(lowerCaseSearch)) &&
+          p.owner !== localStorage.getItem("id"),
       );
 
       if (postsFiltered.length === 0) {
-        setMessage('No se encuentran resultados relacionados');
+        setMessage("No se encuentran resultados relacionados");
       } else {
-        setMessage(''); // Limpiar el mensaje si se encuentran resultados
+        setMessage(""); // Limpiar el mensaje si se encuentran resultados
       }
 
       setPosts(postsFiltered);
     } else {
-      console.warn('No posts available to filter');
-      setMessage('No hay publicaciones disponibles');
+      console.warn("No posts available to filter");
+      setMessage("No hay publicaciones disponibles");
     }
   };
 
   const aplicarFiltros = async () => {
-    const selectOrdenador = document.getElementById('ordenador');
+    const selectOrdenador = document.getElementById("ordenador");
     let posts = await getPosts();
-    posts = posts.filter(p => p.owner !== localStorage.getItem('id'));
+    posts = posts.filter((p) => p.owner !== localStorage.getItem("id"));
 
-    if (!localStorage.getItem('id')  || localStorage.getItem('verified') === 'false' && localStorage.getItem('type') !== 'Admin') {
-      posts = posts.filter(p => p.type === 'Embarcaciones');
+    if (
+      !localStorage.getItem("id") ||
+      (localStorage.getItem("verified") === "false" && localStorage.getItem("type") !== "Admin")
+    ) {
+      posts = posts.filter((p) => p.type === "Embarcaciones");
     }
 
-    posts = posts.filter(p => p.state === 'Activo');
+    posts = posts.filter((p) => p.state === "Activo");
 
-    const activeFilters = Object.keys(selectedOptions).filter(option => selectedOptions[option]);
-    let filteredPosts = posts.filter(post => {
-      return activeFilters.every(filter => {
-        if (filter === 'adaptados') {
+    const activeFilters = Object.keys(selectedOptions).filter((option) => selectedOptions[option]);
+    let filteredPosts = posts.filter((post) => {
+      return activeFilters.every((filter) => {
+        if (filter === "adaptados") {
           return post.adapted === true;
         } else {
           return post.type.toLowerCase() === filter.toLowerCase();
         }
       });
     });
-    
+
     setPosts(filteredPosts);
-    
   };
 
   const handleCheckboxChange = (event) => {
     const { id, checked } = event.target;
-    setSelectedOptions(prevState => ({
+    setSelectedOptions((prevState) => ({
       ...prevState,
       [id]: checked,
     }));
@@ -126,105 +134,158 @@ export default function Home() {
 
   return (
     <>
-      <div className='flex'>
-        <form className="w-1/2 mt-auto mb-auto ml-auto p-3" onSubmit={handleSubmit} ref={form}>
-          <label htmlFor="default-search" className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">
-            Search
-          </label>
-          <div className="relative rounded-xl bg-custom-gray">
-            <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
-              <svg
-                className="w-4 h-4 text-gray-500 dark:text-gray-400"
-                aria-hidden="true"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 20 20"
+      {loading ? (
+        <Loading />
+      ) : (
+        <>
+          <div className="flex">
+            <form className="w-1/2 mt-auto mb-auto ml-auto p-3" onSubmit={handleSubmit} ref={form}>
+              <label
+                htmlFor="default-search"
+                className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white"
               >
-                <path
-                  stroke="currentColor"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
+                Search
+              </label>
+              <div className="relative rounded-xl bg-custom-gray">
+                <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+                  <svg
+                    className="w-4 h-4 text-gray-500 dark:text-gray-400"
+                    aria-hidden="true"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 20 20"
+                  >
+                    <path
+                      stroke="currentColor"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                      d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
+                    />
+                  </svg>
+                </div>
+                <input
+                  type="search"
+                  id="default-search"
+                  className="block w-full p-4 pl-10 text-sm text-gray-900 border border-gray-300 rounded-xl bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 break-words overflow-y-auto"
+                  placeholder="Buscar Embarcaciones, Vehiculos..."
                 />
-              </svg>
+                <button
+                  type="button"
+                  className="text-white absolute right-2.5 bottom-2.5 rounded-xl bg-gray-800 duration-300 hover:bg-gray-700 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                >
+                  Buscar
+                </button>
+              </div>
+            </form>
+            <div className="flex w-1/2 mr-auto p-3 items-center justify-evenly text-sm">
+              <div className="flex items-center justify-between">
+                <div className="mr-1 text-xs">Ordenar Por</div>
+                <select name="ordenador" id="ordenador" className="rounded-xl text-xs justify-evenly">
+                  <option value="nuevos">Más nuevas</option>
+                  <option value="viejos">Más antiguas</option>
+                </select>
+              </div>
+              {localStorage.getItem("type") === "Titular" && localStorage.getItem("verified") === "true" && (
+                <div id="checkboxList" className="text-sm font-medium">
+                  <div>
+                    <input
+                      type="checkbox"
+                      id="Embarcaciones"
+                      className="m-1 rounded-xl focus:outline-none"
+                      style={{ outline: "none", boxShadow: "none" }}
+                      onChange={handleCheckboxChange}
+                    />
+                    <label htmlFor="Embarcaciones">Embarcaciones</label>
+                  </div>
+                  <div>
+                    <input
+                      type="checkbox"
+                      id="Vehículos"
+                      className="m-1 rounded-xl focus:outline-none"
+                      style={{ outline: "none", boxShadow: "none" }}
+                      onChange={handleCheckboxChange}
+                    />
+                    <label htmlFor="Vehículos">Vehiculos</label>
+                  </div>
+                  <div>
+                    <input
+                      type="checkbox"
+                      id="Aeronaves"
+                      className="m-1 rounded-xl focus:outline-none"
+                      style={{ outline: "none", boxShadow: "none" }}
+                      onChange={handleCheckboxChange}
+                    />
+                    <label htmlFor="Aeronaves">Aeronaves</label>
+                  </div>
+                  <div>
+                    <input
+                      type="checkbox"
+                      id="Inmuebles"
+                      className="m-1 rounded-xl focus:outline-none"
+                      style={{ outline: "none", boxShadow: "none" }}
+                      onChange={handleCheckboxChange}
+                    />
+                    <label htmlFor="Inmuebles">Inmuebles</label>
+                  </div>
+                </div>
+              )}
+              <div>
+                <input
+                  type="checkbox"
+                  id="adaptados"
+                  className="rounded-xl focus:outline-none"
+                  style={{ outline: "none", boxShadow: "none" }}
+                  onChange={handleCheckboxChange}
+                />
+                <label htmlFor="adaptados" className="m-1 text-sm font-medium">
+                  Aptos para discapacitados
+                </label>
+              </div>
+              <button
+                className="text-white rounded-xl bg-gray-800 duration-300 hover:bg-gray-700 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                onClick={aplicarFiltros}
+              >
+                Aplicar
+              </button>
             </div>
-            <input
-              type="search"
-              id="default-search"
-              className="block w-full p-4 pl-10 text-sm text-gray-900 border border-gray-300 rounded-xl bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 break-words overflow-y-auto"
-              placeholder="Buscar Embarcaciones, Vehiculos..."
-            />
-            <button
-              type="button"
-              className="text-white absolute right-2.5 bottom-2.5 rounded-xl bg-gray-800 duration-300 hover:bg-gray-700 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-            >
-              Buscar
-            </button>
           </div>
-        </form>
-        <div className='flex w-1/2 mr-auto p-3 items-center justify-evenly text-sm'>
-          <div className='flex items-center justify-between'>
-            <div className='mr-1 text-xs'>Ordenar Por</div>
-            <select
-              name="ordenador"
-              id="ordenador"
-              className='rounded-xl text-xs justify-evenly'>
-              <option value="nuevos">Más nuevas</option>
-              <option value="viejos">Más antiguas</option>
-            </select>
-          </div>
-          {localStorage.getItem("type") === "Titular" && localStorage.getItem("verified") === "true" &&
-            <div id="checkboxList" className='text-sm font-medium'>
-              <div>
-                <input type="checkbox" id="Embarcaciones" className='m-1 rounded-xl focus:outline-none' style={{ outline: 'none', boxShadow: 'none' }} onChange={handleCheckboxChange}/>
-                <label for="Embarcaciones">Embarcaciones</label>
+          <div className="max-w-screen-xl flex flex-wrap justify-between">
+            <div className="w-1/5 p-2 sticky top-0 h-screen flex items-center">
+              <div className="aspect-w-16 aspect-h-9 mb-4">
+                <video
+                  src="/yateMate.mp4"
+                  autoPlay
+                  muted
+                  loop
+                  className="object-cover rounded-xl"
+                  width="320"
+                  height="180"
+                />
               </div>
-              <div>
-                <input type="checkbox" id="Vehículos" className='m-1 rounded-xl focus:outline-none' style={{ outline: 'none', boxShadow: 'none' }} onChange={handleCheckboxChange}/>
-                <label for="Vehículos">Vehiculos</label>
-              </div>
-              <div>
-                <input type="checkbox" id="Aeronaves" className='m-1 rounded-xl focus:outline-none' style={{ outline: 'none', boxShadow: 'none' }} onChange={handleCheckboxChange}/>
-                <label for="Aeronaves">Aeronaves</label>
-              </div>
-              <div>
-                <input type="checkbox" id="Inmuebles" className='m-1 rounded-xl focus:outline-none' style={{ outline: 'none', boxShadow: 'none' }} onChange={handleCheckboxChange}/>
-                <label for="Inmuebles">Inmuebles</label>
-              </div>
-            </div>}
-            <div>
-              <input type="checkbox" id="adaptados" className='rounded-xl focus:outline-none' style={{ outline: 'none', boxShadow: 'none' }} onChange={handleCheckboxChange}/>
-              <label for="adaptados" className='m-1 text-sm font-medium' >Aptos para discapacitados</label>
             </div>
-          <button
-          className="text-white rounded-xl bg-gray-800 duration-300 hover:bg-gray-700 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-          onClick={aplicarFiltros}
-          >
-            Aplicar
-          </button>
-        </div>
-      </div>
-      <div className="max-w-screen-xl flex flex-wrap justify-between">
-        <div className="w-1/5 p-2 sticky top-0 h-screen flex items-center">
-          <div className="aspect-w-16 aspect-h-9 mb-4">
-            <video src="/yateMate.mp4" autoPlay muted loop className="object-cover rounded-xl" width="320" height="180" />
+            <div className="w-4/5 p-2 grid grid-cols-3 gap-4">
+              {posts && posts.length > 0 ? (
+                posts.map((post) => (
+                  <div className="p-2" key={post._id}>
+                    {
+                      <Post
+                        post={post}
+                        showProposeButton={true}
+                        isFavorite={favoritesPosts.includes(post._id) ? true : false}
+                      />
+                    }
+                  </div>
+                ))
+              ) : (
+                <span className="text-3xl mt-auto mb-auto ml-auto mr-auto text-left p-3 col-span-3 font-medium">
+                  {message}
+                </span>
+              )}
+            </div>
           </div>
-        </div>
-        <div className="w-4/5 p-2 grid grid-cols-3 gap-4">
-          {loading ? (
-            <span className="text-3xl mt-auto mb-auto ml-auto mr-auto text-left p-3 col-span-3 font-medium">Cargando Publicaciones...</span>
-          ) : posts && posts.length > 0 ? (
-            posts.map((post) => (
-              <div className="p-2" key={post._id}>
-                {<Post post={post} showProposeButton={true} isFavorite={favoritesPosts.includes(post._id) ? true : false} />}
-              </div>
-            ))
-          ) : (
-            <span className="text-3xl mt-auto mb-auto ml-auto mr-auto text-left p-3 col-span-3 font-medium">{message}</span>
-          )}
-        </div>
-      </div>
+        </>
+      )}
     </>
   );
 }
