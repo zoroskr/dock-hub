5944b585b0c8328b79ca6de7fefa6b77dc1c3782
Felipe <mundofelipote@gmail.com>
2024-07-07 19:09:28 -0300

    ahora si esta arregladogit add .

--- src/components/ui/MapSinDrag.jsx
+++ src/components/ui/MapSinDrag.jsx
@@ -2,11 +2,13 @@
 import React from "react";
 import { MapContainer, TileLayer } from "react-leaflet";
 import "@/components/ui/Map.css";
+import "leaflet/dist/leaflet.css";
+
 
 const MapDisplay = ({ lat, lng }) => {
   return (
     <div className="w-full h-full flex items-center justify-center">
-      <MapContainer center={{ lat, lng }} zoom={13} scrollWheelZoom={false} className="w-full h-full">
+      <MapContainer center={{ lat, lng }} zoom={13} scrollWheelZoom={false}>
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
