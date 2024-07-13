291506de84e4884a8829ac85638ebc3f0633acb0
Alexis Rodriguez <alexisrodriguezarteaga@gmail.com>
2024-07-13 19:38:06 -0300

    Fix

--- src/app/api/verify/route.js
+++ src/app/api/verify/route.js
@@ -3,15 +3,15 @@ import fetch from "node-fetch";
 
 export async function POST(request) {
   const body = await request.json();
-  const response = await fetch("https://pwc9ly0elf.execute-api.sa-east-1.amazonaws.com/default/verify", {
+  const response = await fetch(process.env.API_URL, {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
-      "x-api-key": "TcWtLz6t38akcV9uG1gKg4z4vgM6fbdt4U9BgGXR",
+      "x-api-key": process.env.API_KEY,
     },
     body: JSON.stringify(body),
   });
   const data = await response.json();
-  console.log("🚀 ~ POST ~ data:", data)
+  console.log("🚀 ~ POST ~ data:", data);
   return NextResponse.json(data);
 }
